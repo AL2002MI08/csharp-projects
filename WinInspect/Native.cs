a4ba02d38a0efed14c2b0d9af4c8090304766f00
Alexandra_08 <alexandramizero@gmail.com>
2026-09-22 06:01:44 +0200

    feat: add native win32 bindings for process inspection

---
+++ WinInspect/Native.cs
@@ -0,0 +1,58 @@
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WinInspect;
+
+internal static class Native
+{
+    public const uint PROCESS_QUERY_LIMITED_INFORMATION = 0x1000;
+
+    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+    public static extern IntPtr OpenProcess(
+        uint dwDesiredAccess,
+        [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle,
+        uint dwProcessId);
+
+    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool CloseHandle(IntPtr hObject);
+
+    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+    public static extern bool IsWow64Process(
+        IntPtr hProcess,
+        [MarshalAs(UnmanagedType.Bool)] out bool wow64Process);
+
+    [DllImport("psapi.dll", SetLastError = true, ExactSpelling = true)]
+    public static extern bool GetProcessMemoryInfo(
+        IntPtr hProcess,
+        out PROCESS_MEMORY_COUNTERS counters,
+        uint cb);
+
+    [DllImport("user32.dll", SetLastError = true, ExactSpelling = true)]
+    public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+    [DllImport("user32.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
+    public static extern int GetWindowTextW(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+    [DllImport("user32.dll", ExactSpelling = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool IsWindowVisible(IntPtr hWnd);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct PROCESS_MEMORY_COUNTERS
+    {
+        public uint cb;
+        public uint PageFaultCount;
+        public nuint PeakWorkingSetSize;
+        public nuint WorkingSetSize;
+        public nuint QuotaPeakPagedPoolUsage;
+        public nuint QuotaPagedPoolUsage;
+        public nuint QuotaPeakNonPagedPoolUsage;
+        public nuint QuotaNonPagedPoolUsage;
+        public nuint PagefileUsage;
+        public nuint PeakPagefileUsage;
+    }
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+}
